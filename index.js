70055b14987e2b239b70b05c591a4b3f993c6954
SergeiVavilin <>
2023-03-17 07:51:40 +0000

    последняя +-версия, не нравится, что криво работает с числами больше 10, заебемся выдавать результат, буду переписывать функции конвертации из римских и в римские

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inputString = 'V+V';
+const inputString = 'X+X';
 const romanToArabicAlphabet = {
   "I": "1",
   "II": "2",
@@ -10,14 +10,25 @@ const romanToArabicAlphabet = {
   "VIII": "8",
   "IX": "9",
   "X": "10",
+  "XI": "11",
+  "XII": "12",
+  "XIII": "13",
+  "XIV": "14",
+  "XV": "15",
+  "XVI": "16",
+  "XVII": "17",
+  "XVIII": "18",
+  "XIX": "19",
+  "XX": "20",
   "*": "*",
   "+": "+",
   "/": "/",
   "-": "-"
-}; //alphabet
-//const myRegex = /^[1-9]\s*[\+\-\*\/]\s*[1-10]$|^10\s*[\+\-\*\/]\s*[1-10]$|^[1-10]\s*[\+\-\*\/]\s*10$/;
-const myRegex = /^([1-9]|10)\s*[\+\-\*\/]\s*([1-9]|10)$/;
-//регулярка для проверки араб-символ-араб
+}; //алфавит
+
+const mainRegex = /^([1-9]|10)\s*[\+\-\*\/]\s*([1-9]|10)$/;
+//регулярка для проверки араб-символ-араб, операнды макс 10
+
 function isArabicOrRoman(str) {
   if (/^[\d+\-*/.\s]+$/.test(str)) {
     return 'arabic';    
@@ -28,33 +39,77 @@ function isArabicOrRoman(str) {
   }
 } //на этом этапе код определяет невзирая на символы арабская или римская строка
 
-//дальше надо написать блок кода, который будет учитывая работу функции isArabicOrRoman разделять вычисления, первое что надо сделать допилить арабские символы и проверить будет ли считать
-//console.log(isArabicOrRoman(inputString));
+
   switch (isArabicOrRoman(inputString)){
+      
     case 'arabic':
-      if (myRegex.test(inputString)) {
+      if (mainRegex.test(inputString)) {
         const result = eval(inputString);
-        console.log(result); // Output: 14
-        break;
+        console.log(Math.floor(result)); // допилить округление
+      } else {
+        console.log('error arabic more then 10');
       }
+      break;
+      
     case 'roman':
-      function convertToArabic(inputString) {
-        const splitString = inputString.split(' ');
+      
+      function convertToArabic(string) {
+        const splitString = string.split(/([-/*+])/); // Используем регулярное выражение, чтобы разделить строку по символам -/*+
         const convertedToArabic = splitString.map(char => {
           if (Object.keys(romanToArabicAlphabet).includes(char)) {
             return romanToArabicAlphabet[char];
           } else {
             return char;
           }
-        }).join(' ');
-  
-        console.log(eval(convertedToArabic));
+        }).join(''); 
+        return convertedToArabic;
+      };
+      
+      function convertToRomanian(string) {
+        const splitString = inputString.split('');
+        const convertedToRomanian = splitString.map(char => {
+          if (Object.values(romanToArabicAlphabet).includes(char)) {
+            return Object.keys(romanToArabicAlphabet).find(key => romanToArabicAlphabet[key] === char);
+          } else {
+            return char;
+          }
+        }).join('');
+        return convertedToRomanian;
       }
+      console.log(convertToRomanian(inputString));
+      
+      let convertedToArabic = convertToArabic(inputString);
+      console.log(convertedToArabic+'конвертировали в арабские');
+
+      function isMoreOrEqualZero(number) {
+        if (mainRegex.test(number)) {
+          const result = eval(number);
+          return Math.floor(result);
+          console.log(Math.floor(result)); //округление вниз
+        } else {
+          return ('error arabic more then 10');
+        }
+      };
+      console.log(isMoreOrEqualZero(convertedToArabic)+'проверили меньше ли нуля');
+      let result = (isMoreOrEqualZero(convertedToArabic));
+      let resultInRomanian = convertToRomanian(result);
+      console.log(resultInRomanian + 'обратно в римские');
+      
+      /*if (mainRegex.test(convertedToArabic)) {
+        const result = eval(convertedToArabic);
+        console.log(result); // допилить округление
+      } else {
+        console.log('error arabic more then 10');
+      };     
+      if ()*/
       break;
+      
     default:
-      console.log('error');
+      console.log('error mixed numbers');
       
   }
+
+
 
 
 
